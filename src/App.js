aa93be88a049be6bbc2600348526a0218fcd1ab8
janbornindec <janellecwx@gmail.com>
2024-10-13 18:58:08 +1000

    First working version. Need styling.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/SearchBar';
+import Result from './components/Result';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <Result />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Result = () => {
+    return (
+        <div>
+            <h2>Results</h2>
+        </div>
+    )
+}
+
+export default Result;
